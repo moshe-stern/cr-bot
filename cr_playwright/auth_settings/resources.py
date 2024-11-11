c16636d6c2d00f89c9ff944f7c59594b24923d85
moshe-stern <guntheramassed@gmail.com>
2024-11-11 11:21:31 -0500

    update

--- cr_playwright/auth_settings/resources.py
+++ cr_playwright/auth_settings/resources.py
@@ -2,15 +2,15 @@ import dataclasses
 from typing import List
 
 @dataclasses.dataclass
-class Resource:
+class CRResource:
     id: int
     codes_to_remove: List[str]
     codes_to_add: List[str]
 
 
-test_resource = Resource(
-    50704127,
-    ['97151: ASMT/Reassessment', '97151: Assessment, Lic/Cert only - REVIEWER'],
-    ['97152: Additional Assessment by BCBA']
-)
-resources_to_update = [test_resource]
+# test_resource = Resource(
+#     50704127,
+#     ['97151: ASMT/Reassessment', '97151: Assessment, Lic/Cert only - REVIEWER'],
+#     ['97152: Additional Assessment by BCBA']
+# )
+# resources_to_update = [test_resource]
--- main.py
+++ main.py
@@ -1,44 +1,14 @@
-import os
-
-from flask import Flask, request, abort, jsonify
+from flask import Flask
 from dotenv import load_dotenv
-import pandas as pd
+
+from cr_playwright.auth_settings.controller import update_auth_settings
 
 if not load_dotenv():
     raise Exception('could not import env file')
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'hi'
 @app.route('/auth-settings', methods=['POST'])
-def update():
-    if request.headers.get("X-Secret-Key") != os.getenv('SECRET_KEY'):
-        abort(403)
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part in the request"}), 400
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return jsonify({"error": "No file selected"}), 400
-
-    # Check if the file is an Excel file
-    if not file.filename.endswith('.xlsx'):
-        return jsonify({"error": "Invalid file format. Please upload an .xlsx file"}), 400
-
-    try:
-        # Read the Excel file using pandas
-        df = pd.read_excel(file, engine='openpyxl')
-
-        # Process the DataFrame as needed. For example, converting it to JSON format:
-        data = df.to_dict(orient='records')
-
-        # Return the processed data as JSON
-        return jsonify({"data": data}), 200
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+def update_auth_settings():
+    update_auth_settings()
 
 
 if __name__ == '__main__':
