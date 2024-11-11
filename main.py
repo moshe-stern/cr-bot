8c1a226ca5b603f31605fb3aaf061c2b8c92cb23
moshe-stern <guntheramassed@gmail.com>
2024-11-11 12:08:58 -0500

    update

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 @app.route('/auth-settings', methods=['POST'])
 def update_auth_settings():
     update_auth_settings()
-
+@app.route('/', methods=['GET'])
+def index():
+    return 'App is Working!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
