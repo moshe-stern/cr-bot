a3501ad3143939919cdbb83f8d4deea149725bcd
moshe-stern <guntheramassed@gmail.com>
2024-11-21 10:54:09 -0500

    cleanup queue

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 python -m playwright install
 python -m playwright install-deps
-gunicorn --bind=0.0.0.0:8000 --access-logfile - --error-logfile - --log-level info main:app
-celery -A celery_app.celery worker --loglevel=info --pool=solo
+gpytunicorn --bind=0.0.0.0:8000 --access-logfile - --error-logfile - --log-level info main:app
+celery -A celery_app worker --loglevel=info -Q cr-bot-queue,cleanup_file  --concurrency=1 #if windows --pool=solo
