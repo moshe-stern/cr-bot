2e9b24adc55f6e89433eb572b44e68b7e1a9b0e1
moshe-stern <guntheramassed@gmail.com>
2024-12-09 13:12:07 -0500

    took away post

--- src/session.py
+++ src/session.py
@@ -66,7 +66,6 @@ class CRSession(requests.Session):
         payload = {"token": self.cr_token_response.access_token}
         headers = {"Accept": "application/json", "Content-Type": "application/json"}
         response = self.post(url, headers=headers, json=payload)
-        self.context.post(url, headers=headers, data=payload)
         if response.status_code >= 400:
             raise Exception(
                 f"could not get cookies status code: {response.status_code}"
