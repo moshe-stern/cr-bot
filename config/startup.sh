f4686edcbddf55a175514dcf2eeaaab67a2b7bfa
moshe-stern <guntheramassed@gmail.com>
2024-12-10 15:26:24 -0500

    back

--- config/startup.sh
+++ config/startup.sh
@@ -2,3 +2,5 @@ python -m playwright install
 python -m playwright install-deps
 gunicorn --bind=0.0.0.0:8000 --log-level info main:app
 celery -A celery_app worker --loglevel=info -Q cr-bot-queue,cleanup_file  --concurrency=1 #if windows --pool=solo
+sudo redis-cli FLUSHALL &&
+pgrep -f 'celery' && sudo pkill -9 -f 'celery' &&
