07115343d8ca58125a64503d1d64b7d8ef39ffb8
moshe-stern <guntheramassed@gmail.com>
2025-01-03 10:32:53 -0500

    update-file-struct

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 from flask import Flask
 
 from src.controllers.authorization_controller import authorization
+from src.controllers.celery_controller import celery_controller
 from src.services.shared import logger, register_error_handlers
 
 if not load_dotenv():
     raise Exception("Failed to load dotenv")
 app = Flask(__name__)
 register_error_handlers(app)
+app.register_blueprint(celery_controller)
 app.register_blueprint(authorization)
 
 
--- src/controllers/authorization_controller.py
+++ src/controllers/authorization_controller.py
@@ -1,18 +1,13 @@
 import asyncio
 import os
-from typing import Any
-from celery.result import AsyncResult
-from flask import Blueprint, Response, jsonify, request, send_file
+from flask import Blueprint, Response, jsonify, request
 from playwright.async_api import async_playwright
-
-from celery_app import celery
-from src.services.celery_tasks import process_update, cleanup_file, start_playwright
+from src.services.celery_tasks import process_update, start_playwright
 from src.classes import (
     UpdateType,
 )
 from src.services.shared import (
     get_json,
-    get_task_progress,
     logger,
     divide_list,
     get_resource_arr,
@@ -35,59 +30,6 @@ def update() -> tuple[Response, int]:
     args = (base64_content, update_type_str, instance)
     task = process_update.apply_async(args=args)
     return jsonify({"task_id": task.id}), 202
-
-
-@authorization.route("/status/<task_id>", methods=["GET"])
-def check_task_status(task_id) -> dict[str, Any]:
-    task: AsyncResult = celery.AsyncResult(task_id)
-    response: dict[str, Any] = {}
-    if task.state == "PENDING":
-        response = {
-            "state": task.state,
-            "progress": get_task_progress(task),
-        }
-    elif task.state == "SUCCESS":
-        response = {
-            "state": task.state,
-            "file_url": f"/authorization/download/{task_id}",
-            "progress": "100%",
-        }
-    elif task.state == "FAILURE":
-        reason = task.info.get("reason")
-        task.forget()
-        response = {"state": task.state, "fail_reason": reason}
-    return response
-
-
-@authorization.route("/download/<task_id>", methods=["GET"])
-def download_file(task_id) -> Response | tuple[Response, int]:
-    task = celery.AsyncResult(task_id)
-    if task.state == "SUCCESS":
-        try:
-            if isinstance(task.result, str):
-                file_path = task.result
-                response = send_file(
-                    file_path,
-                    as_attachment=True,
-                    download_name="updated_file.xlsx",
-                    mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                )
-            else:
-                raise Exception("Task result failed")
-            cleanup_file.apply_async((file_path,), countdown=30)
-            return response
-        except Exception as e:
-            return jsonify({"error": f"Error serving file: {str(e)}"}), 500
-        finally:
-            task.forget()
-
-    elif task.state == "PENDING":
-        return jsonify({"error": "Task is still pending"}), 202
-    elif task.state == "FAILURE":
-        task.forget()
-        return jsonify({"error": "Task has failed"}), 400
-    else:
-        return jsonify({"error": "Task not ready or invalid"}), 400
 
 
 # for quick testing when adding new services
